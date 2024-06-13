0598c27ec7222f83033a205f4e29d72e90def198
Josep Vives Amat <jvives@gmail.com>
2024-06-13 10:36:26 +0200

    Refactor + Add input parameters

--- Program.cs
+++ Program.cs
@@ -1,53 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Drawing;
-using ScottPlot;
+﻿using ScottPlot;
+using System.Data;
 
 namespace MonteCarloSimulation
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Input data: past days and the number of delivered items for each day
-            List<int> pastDeliveredItems = new List<int> { 45, 19, 42, 37, 26, 49, 48, 31, 52};
+            int numberOfSimulations;
+            int periodsPerRound;
 
-            int numberOfSimulations = 10000;
-            int periodsPerRound = 3;
+            if (!CheckInputParameters(args, out numberOfSimulations, out periodsPerRound))
+                return;
+            
+            List<int> pastDeliveredItems = LoadHistoric();
+            if (pastDeliveredItems.Count==0)
+                return;
+
+            // Run Monte Carlo simulations
+            List<int> simulationResults = RunSimulations(numberOfSimulations, periodsPerRound, pastDeliveredItems);
+
+            // Create bar chart and save as image
+            CreateBarChart(simulationResults, "MonteCarloResults.png");
+
+            // Calculate and print percentiles
+            PrintPercentiles(simulationResults);
+        }
+
+        static void PrintPercentiles(List<int> simulationResults){
+            Console.WriteLine($"50th Percentile: {GetPercentile(simulationResults, 50)}");
+            Console.WriteLine($"10th Percentile: {GetPercentile(simulationResults, 10)}");
+            Console.WriteLine($"15th Percentile: {GetPercentile(simulationResults, 15)}");
+            Console.WriteLine($"20th Percentile: {GetPercentile(simulationResults, 20)}");
+            Console.WriteLine($"25th Percentile: {GetPercentile(simulationResults, 25)}");
+        }
+
+        static bool CheckInputParameters(string[] args, out int numberOfSimulations, out int periodsPerRound){
+            numberOfSimulations=10000; // default value
+            periodsPerRound =1; // default value
+            if (args.Length!=3){
+                Console.WriteLine("Wrong number of input parameters.");
+                return false;
+            }
+            
+            if (!Int32.TryParse(args[1], out numberOfSimulations)){
+                Console.WriteLine("Wrong value for parameter 'Number of Simulations'");
+                return false;
+            }
+
+            if (!Int32.TryParse(args[2], out periodsPerRound)){
+                Console.WriteLine("Wrong value for parameter 'Number of Periods in each scenario'");
+                return false;
+            }
+            return true;
+        }
+
+        static List<int> RunSimulations(int numberOfSimulations, int periodsPerRound, List<int> pastDeliveredItems){
             List<int> simulationResults = new List<int>();
-
             Random rand = new Random();
 
             // Run Monte Carlo simulations
-            for (int i = 0; i < numberOfSimulations; i++)
-            {
+            for (int i = 0; i < numberOfSimulations; i++){
                 int totalDelivered = 0;
-                for (int j = 0; j < periodsPerRound; j++)
-                {
+                for (int j = 0; j < periodsPerRound; j++){
                     int randomIndex = rand.Next(pastDeliveredItems.Count);
                     totalDelivered += pastDeliveredItems[randomIndex];
                 }
                 simulationResults.Add(totalDelivered);
             }
-
-            // Sort results to calculate percentiles
-            simulationResults.Sort();
             
-            // Calculate and print percentiles
-         Console.WriteLine($"50th Percentile: {GetPercentile(simulationResults, 50)}");
-            // Console.WriteLine($"75th Percentile: {GetPercentile(simulationResults, 75)}");
-            // Console.WriteLine($"80th Percentile: {GetPercentile(simulationResults, 80)}");
-            // Console.WriteLine($"90th Percentile: {GetPercentile(simulationResults, 90)}");
-
-            Console.WriteLine($"10th Percentile: {GetPercentile(simulationResults, 10)}");
-            Console.WriteLine($"15th Percentile: {GetPercentile(simulationResults, 15)}");
-            Console.WriteLine($"20th Percentile: {GetPercentile(simulationResults, 20)}");
-            Console.WriteLine($"25th Percentile: {GetPercentile(simulationResults, 25)}");
-
-
-            // Create bar chart and save as image
-            CreateBarChart(simulationResults, "MonteCarloResults.png");
+            // Sort results --> handy to calculate percentiles
+            simulationResults.Sort();
+            return simulationResults;
         }
 
         static int GetPercentile(List<int> sortedResults, double percentile)
@@ -56,6 +80,30 @@ namespace MonteCarloSimulation
             return sortedResults[index];
         }
 
+        private static List<int> LoadHistoric(){
+            
+            // Read historical data
+            List<int> pastDeliveredItems = new List<int>();
+            
+            try{
+                StreamReader reader = new StreamReader(@".\historic.csv");
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line?.Split(',');
+                    var isNumeric = Int32.TryParse(values?[1], out int deliveredItems);
+                    
+                    if (isNumeric) {
+                        pastDeliveredItems.Add(deliveredItems);
+                        Console.WriteLine(deliveredItems);
+                    }
+                }
+            }
+            catch (Exception){
+                Console.WriteLine("Error opening input file.");
+            }
+            return pastDeliveredItems;
+        }
         static void CreateBarChart(List<int> results, string filePath)
         {
             // Count frequency of each result
@@ -67,18 +115,15 @@ namespace MonteCarloSimulation
             var labels = new List<string>();
 
             int i = 0;
-            foreach (var item in frequency.OrderBy(x => x.Key))
-            {
+            foreach (var item in frequency.OrderBy(x => x.Key)){
                 values.Add(item.Value);
                 positions.Add(i);
                 labels.Add(item.Key.ToString());
                 i++;
-                //Console.WriteLine($"{item.Key.ToString()}:{item.Value}");
             }
 
             // Create the plot
-            var plt = new ScottPlot.Plot();
-            
+            var plt = new Plot();
             plt.Resize(3000, 2000);
 
             // Add bar chart
